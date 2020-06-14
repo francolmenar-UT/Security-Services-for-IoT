7db8fadabff0d542e31a1e675643291591557a1d
Raphael <19925795+RaphEnio@users.noreply.github.com>
2020-06-14 18:26:04 +0200

    added file to convert pcap to csv

---
+++ Final-Project/covertfiles.py
@@ -0,0 +1,21 @@
+import glob
+import re
+import subprocess
+import os
+#load filenames of all pcap files
+path = "C:\\Users\\Raphael\\github\\Security-Services-for-IoT\\Final-Project\\pcap\\raw\\MiLamp\\"
+out_path = "C:\\Users\\Raphael\\github\\Security-Services-for-IoT\\Final-Project\\pcap\\processed\\csv-files\\"
+filenames = (glob.glob(path + "*.pcapng"))
+
+#extract filenames
+testname = []
+for file in filenames:
+    names = file.split("\\")
+    testname.append(names[len(names) -1].split(".")[0])
+
+#conver pcap files to csv and save them under the same name as the pcap file
+for i in range(len(filenames)):
+    command = "tshark -r" + filenames[i] + " -T fields -e frame.number -e frame.time -e eth.src -e eth.dst" + \
+            "-e ip.src -e ip.dst -e ip.proto -e data.data -e data.len -e udp.srcport -e udp.dstport -e udp.length " + \
+            "-e eth.addr.oui_resolved -E header=y -E separator="","" -E quote=d -E occurrence=f > " + out_path + testname[i] + ".csv"
+    os.system(command)
