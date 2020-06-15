9b0b37ba4f1bbde59c6f96b981e94816a74e2785
Raphael <19925795+RaphEnio@users.noreply.github.com>
2020-06-15 15:15:23 +0200

    new stuff

--- Final-Project/covertfiles.py
+++ Final-Project/covertfiles.py
@@ -4,6 +4,8 @@ import subprocess
 import os
 #load filenames of all pcap files
 path = "C:\\Users\\Raphael\\github\\Security-Services-for-IoT\\Final-Project\\pcap\\raw\\MiLamp\\"
+#path = "C:\\Users\\Raphael\\github\\Security-Services-for-IoT\\Final-Project\\pcap\\raw\\Smart_Plug-HS100\\"
+#out_path = "C:\\Users\\Raphael\\github\\Security-Services-for-IoT\\Final-Project\\pcap\\processed\\splug-csv\\"
 out_path = "C:\\Users\\Raphael\\github\\Security-Services-for-IoT\\Final-Project\\pcap\\processed\\csv-files\\"
 filenames = (glob.glob(path + "*.pcapng"))
 
@@ -15,7 +17,7 @@ for file in filenames:
 
 #conver pcap files to csv and save them under the same name as the pcap file
 for i in range(len(filenames)):
-    command = "tshark -r" + filenames[i] + " -T fields -e frame.number -e frame.time -e eth.src -e eth.dst" + \
+    command = "tshark -r" + filenames[i] + " -T fields -e frame.number -e frame.time -e eth.src -e eth.dst " + \
             "-e ip.src -e ip.dst -e ip.proto -e data.data -e data.len -e udp.srcport -e udp.dstport -e udp.length " + \
-            "-e eth.addr.oui_resolved -E header=y -E separator="","" -E quote=d -E occurrence=f > " + out_path + testname[i] + ".csv"
+            "-e eth.addr.oui_resolved -e tcp.srcport  -e tcp.dstport -e tcp.len -E header=y -E separator="","" -E quote=d -E occurrence=f > " + out_path + testname[i] + ".csv"
     os.system(command)
